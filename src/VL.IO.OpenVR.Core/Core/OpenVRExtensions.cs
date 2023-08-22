bf26679cb49a21e4ac08d5eca6041d94d93c0853
bj-rn <schwarzer@posteo.net>
2023-08-22 10:41:19 +0200

    uses simple ToVector3 method. which also seems to be a lot faster.

--- src/VL.IO.OpenVR.Core/Core/OpenVRExtensions.cs
+++ src/VL.IO.OpenVR.Core/Core/OpenVRExtensions.cs
@@ -44,19 +44,10 @@ namespace VL.IO.ValveOpenVR
         }
 
 
-        private static Vector3 UnsafeToVector3(this HmdVector3_t vector)
-        {
-            Debug.Assert(Unsafe.SizeOf<Vector3>() == Unsafe.SizeOf<HmdVector3_t>());
-
-            var v = new Vector3();
-
-            Unsafe.As<Vector3, HmdVector3_t>(ref v) = vector;
-
-            return v;
-        }
-
         public static Vector3 ToVector3(this HmdVector3_t vector)
-        { return UnsafeToVector3(vector); }
+        { 
+            return new Vector3(vector.v0, vector.v1, vector.v2); 
+        }
 
 
 
